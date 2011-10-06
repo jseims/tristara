ef6fec48e6b8d6dad1845658f73653bc30694e0e
Joshua Seims <jseims@hotmail.com>
2011-10-06 02:19:21 +0000

    fleshing out the text

--- server/tristara/views.py
+++ server/tristara/views.py
@@ -2,5 +2,6 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-def index(request):
-    return render_to_response('index.html', {}, context_instance=RequestContext(request))
+def static(request, template):
+    return render_to_response(template, {}, context_instance=RequestContext(request))
+    
--- server/urls.py
+++ server/urls.py
@@ -5,6 +5,10 @@ from django.conf.urls.defaults import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', 'tristara.views.index'),
+    (r'^$', 'tristara.views.static', {'template' : 'index.html'}),
+    (r'^projects$', 'tristara.views.static', {'template' : 'projects.html'}),
+    (r'^work$', 'tristara.views.static', {'template' : 'work.html'}),
+    (r'^about$', 'tristara.views.static', {'template' : 'about.html'}),
+    (r'^contact$', 'tristara.views.static', {'template' : 'contact.html'}),
 
 )
