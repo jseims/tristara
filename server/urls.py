36133a9aeab9b9e8cba83b431647ae42399cf090
Joshua Seims <jseims@hotmail.com>
2011-10-12 00:27:05 +0000

    more copy

--- server/urls.py
+++ server/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('',
     (r'^$', 'tristara.views.static', {'template' : 'index.html'}),
     (r'^projects$', 'tristara.views.static', {'template' : 'projects.html'}),
-    (r'^work$', 'tristara.views.static', {'template' : 'work.html'}),
+    (r'^services$', 'tristara.views.static', {'template' : 'work.html'}),
     (r'^about$', 'tristara.views.static', {'template' : 'about.html'}),
     (r'^contact$', 'tristara.views.static', {'template' : 'contact.html'}),
 
